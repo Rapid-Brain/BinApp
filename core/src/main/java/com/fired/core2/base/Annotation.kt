6f3427509f500236455e120c91b08b3854e7b4b6
Yahya Hassani <yahyamh01@gmail.com>
2022-11-12 17:34:20 +0100

    Update README.md

--- core/src/main/java/com/fired/core2/base/Annotation.kt
+++ core/src/main/java/com/fired/core2/base/Annotation.kt
@@ -8,11 +8,11 @@ import androidx.compose.ui.tooling.preview.Preview
  * @since 12th November 2022
  */
 
-@Preview(name = "phone", device = Devices.PHONE)
-@Preview(name = "foldable", device = Devices.FOLDABLE)
-@Preview(name = "custom", device = "spec:width=1280dp, height=800dp,dpi=480")
-@Preview(name = "tablet", device = Devices.TABLET)
-@Preview(name = "desktop", device = "id:desktop_medium")
+@Preview(showSystemUi = true, name = "phone", device = Devices.PHONE)
+@Preview(showSystemUi = true, name = "foldable", device = Devices.FOLDABLE)
+@Preview(showSystemUi = true,name = "custom", device = "spec:width=1280dp, height=800dp,dpi=480")
+@Preview(showSystemUi = true,name = "tablet", device = Devices.TABLET)
+@Preview(showSystemUi = true,name = "desktop", device = "id:desktop_medium")
 annotation class ReferenceDevices
 
 
